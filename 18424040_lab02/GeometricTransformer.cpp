bd083bc149a93dbf285a100388fbd1302631aaf9
Lê Hoàng Luật <dr.goodperry@gmail.com>
2020-05-10 22:14:49 +0700

    revise code

--- 18424040_lab02/GeometricTransformer.cpp
+++ 18424040_lab02/GeometricTransformer.cpp
@@ -564,14 +564,14 @@ int GeometricTransformer::Flip(const Mat & srcImage, Mat & dstImage, bool direct
 	if (direction)
 	{
 		//giảm đi 1 do duyệt từ 0
-		affine.Translate(0, height - 1);
-		affine.Scale(1, -1);
+		affine.Translate(width - 1, 0);
+		affine.Scale(-1, 1);
 	}
 	else
 	{
 		//giảm đi 1 do duyệt từ 0
-		affine.Translate(width - 1, 0);
-		affine.Scale(-1, 1);
+		affine.Translate(0, height - 1);
+		affine.Scale(1, -1);
 	}
 	
 	dstImage = Mat(height, width, CV_8UC3, Scalar(0));
